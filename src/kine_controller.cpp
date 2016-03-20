d744678fd7c0d70e6054f16c8af16bba79d01a3e
paulachristiny <paula.christiny.silva@usp.br>
2016-03-20 15:12:36 -0300

    subscritor do twist

--- src/kine_controller.cpp
+++ src/kine_controller.cpp
@@ -1,33 +1,46 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Twist.h>
-int main (int argc, char **argv)
-{
-    double v=1, w =0;
-    double b=0.35,R=0.1;
-    //Start ROS within this node
-    ros::init(argc, argv, "controller");
-    //declaring node
-    ros::NodeHandle node;
-    //Declare topic to be published
-    ros::Publisher pub_right = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed", 1);
-    ros::Publisher pub_left = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed", 1);
+
+double v,w;
+ros::Publisher pub_right;
+ros::Publisher pub_left ;
+double b=0.35,R=0.1;
+std_msgs::Float64 wl,wr;
+
+void speedCallBack(const geometry_msgs::TwistConstPtr &msg){
     
-    //Define ROS loop_rate
-    ros::Rate loop_rate(60);
-    //Define message
-    std_msgs::Float64 wl,wr;
-    while(ros::ok()){
+    ROS_INFO("Received message, %f %f", msg->linear.x, msg->angular.z);
+    
+
         //Compute wr and wl
-        wr.data = (v + (b/2)*w)/R;
-        wl.data = (v - (b/2)*w)/R;
+        wr.data = (msg->linear.x + (b/2)*msg->angular.z)/R;
+        wl.data = (msg->linear.x - (b/2)*msg->angular.z)/R;
         //Publish
         ROS_INFO("Publishing angular motors speeds...");
         pub_right.publish(wr) ;
         pub_left.publish(wl) ;
         //ROS routine
-        ros::spinOnce;
-        loop_rate.sleep();
+     
         
-    }
+}
+
+int main (int argc, char **argv)
+{
+
+    
+    //Start ROS within this node
+    ros::init(argc, argv, "controller");
+    //declaring node
+    ros::NodeHandle node;
+    //receive velocity
+    ros::Subscriber twist_sub = node.subscribe<geometry_msgs::Twist>("obstacle/Twist",1, speedCallBack);
+    //Declare topic to be published
+    pub_right = node.advertise<std_msgs::Float64>("vrep/vehicle/motorRightSpeed", 1);
+    pub_left = node.advertise<std_msgs::Float64>("vrep/vehicle/motorLeftSpeed", 1);
+    
+    //Define ROS loop_rate
+    ros::spin();
+    //Define message
+
 }
